d6a0f5eefe84dd850a5406c896da9ea4dafa391a
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2024-08-10 23:40:20 -0400

    Adding post.py to manage the resources from reddit

---
+++ post.py
@@ -0,0 +1,53 @@
+import praw
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Configura tus credenciales de Reddit
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+user_agent = os.getenv('USER_AGENT')
+
+# Autenticación con Reddit
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+
+def get_posts(query, subreddit, limit=100):
+  # Buscar posts en Reddit
+  posts = []
+  for submission in reddit.subreddit(subreddit).search(query, limit=100):
+      posts.append({
+          'title': submission.title,
+          'selftext': submission.selftext,
+          'author': str(submission.author),
+          'score': submission.score,
+          'created_utc': submission.created_utc,
+          'num_comments': submission.num_comments,
+          'url': submission.url
+      })
+
+  # Convertir los posts en un DataFrame de Pandas
+  df = pd.DataFrame(posts)
+
+  # Convertir timestamps a fechas legibles
+  df['created_utc'] = pd.to_datetime(df['created_utc'], unit='s')
+
+  # Eliminar filas con valores nulos en las columnas 'title' y 'selftext'
+  df.dropna(subset=['title', 'selftext'], inplace=True)
+
+  # Eliminar duplicados basados en los títulos de los posts
+  df.drop_duplicates(subset=['title'], inplace=True)
+
+  return df
+
+def get_post_url(url):
+  # Obtener información del post a partir de su URL
+  submission = reddit.submission(url=url)
+
+  post = {
+      'title': submission.title,
+      'selftext': submission.selftext
+  }
+  return post
