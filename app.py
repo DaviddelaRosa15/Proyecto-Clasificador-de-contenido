b09f9a462a2badcbe46c407affd4407ae5d1d212
juancgzmn <82336583+juancgzmn@users.noreply.github.com>
2024-08-18 23:54:33 +0000

    add tabs and change objetive

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from counter import get_country_mentions
 from model import train_df, accuracy_train, accuracy_test, unique_targets, cm_train_df, cm_test_df, predict_from_url, predict_from_text
 from confusion_matrix import plot_confusion_matrix
 
+# Sección 1: Explicación del Proyecto
+# Título del sitio
+st.title('Clasificación de Noticias Latinoamericanas')
+st.write("""
+Este proyecto tiene como objetivo clasificar noticias con respecto a los países latinoamericanos en los medios de comunicación, con el fin de analizar e identificar las áreas de mayor cobertura mediática.
+""")
+
 # Definir la búsqueda
 query = 'latinoamericans countries'
 subreddit = 'all'
@@ -54,163 +61,163 @@ publication_dates_fig.update_layout(
     width=800
 )
 
-# Título del sitio
-st.title('Clasificación de Noticias Latinoamericanas')
+# Crear las pestañas
+tab1, tab2, tab4, tab3, tab5 = st.tabs(["Visualizaciones del EDA", "Datos de Entrenamiento y Precisión del Modelo", "Resultados", "Clasificación", "Acerca De"])
 
-# Sección 1: Explicación del Proyecto
-st.header('Sobre el Proyecto')
-st.write("""
-Este proyecto tiene como objetivo clasificar contenido de noticias en diferentes categorías utilizando un modelo de aprendizaje automático.
-A lo largo de este sitio, exploraremos los datos, visualizaremos el análisis exploratorio de datos (EDA) y proporcionaremos una interfaz
-para que los usuarios interactúen con el modelo y clasifiquen nuevas noticias.
-""")
 
-# Sección 2: Visualizaciones del EDA
-st.header('Análisis Exploratorio de Datos (EDA)')
-st.write("En esta sección, presentamos algunos gráficos que nos ayudan a entender mejor la distribución y las características de las noticias recopiladas.")
+# Tab 1: EDA
+with tab1:
+    # Sección 1: Visualizaciones del EDA
+    st.header('Análisis Exploratorio de Datos (EDA)')
+    st.write("En esta sección, presentamos algunos gráficos que nos ayudan a entender mejor la distribución y las características de las noticias recopiladas.")
 
-# Primer gráfico
-st.subheader('Gráfico de Barras: Presencia de los Países en los Medios')
-st.write("El primer gráfico muestra la presencia de los países latinoamericanos en los medios de comunicación. Este análisis nos permite visualizar qué países tienen más menciones en las noticias, lo cual puede reflejar la relevancia mediática de estos países en un período específico.")
-st.plotly_chart(country_mentions_fig)
+    # Primer gráfico
+    st.subheader('Gráfico de Barras: Presencia de los Países en los Medios')
+    st.write("El primer gráfico muestra la presencia de los países latinoamericanos en los medios de comunicación. Este análisis nos permite visualizar qué países tienen más menciones en las noticias, lo cual puede reflejar la relevancia mediática de estos países en un período específico.")
+    st.plotly_chart(country_mentions_fig)
 
-# Segundo gráfico   
-st.subheader('Gráfico de Barras: Fechas de las publicaciones')
-st.write("El segundo gráfico analiza la distribución temporal de las noticias recopiladas. Al observar las fechas de publicación, podemos identificar patrones temporales, como picos de actividad mediática, que pueden estar relacionados con eventos importantes o tendencias en la región.")
-st.plotly_chart(publication_dates_fig)
+    # Segundo gráfico   
+    st.subheader('Gráfico de Barras: Fechas de las publicaciones')
+    st.write("El segundo gráfico analiza la distribución temporal de las noticias recopiladas. Al observar las fechas de publicación, podemos identificar patrones temporales, como picos de actividad mediática, que pueden estar relacionados con eventos importantes o tendencias en la región.")
+    st.plotly_chart(publication_dates_fig)
 
-# Sección 3: Datos de Entrenamiento y Precisión del Modelo
-st.header('Datos de Entrenamiento y Precisión del Modelo')
+# Tab 3: Datos de Entrenamiento y Precisión del Modelo
+with tab2:
+    # Sección 2: Datos de Entrenamiento y Precisión del Modelo
+    st.header('Datos de Entrenamiento y Precisión del Modelo')
 
-# Ejemplo de datos de entrenamiento
-st.subheader('Resumen de los Datos de Entrenamiento')
-st.write("Aquí se presentan algunos datos usados para entrenar el modelo:")
-st.write(train_df.head(10))
+    # Ejemplo de datos de entrenamiento
+    st.subheader('Resumen de los Datos de Entrenamiento')
+    st.write("Aquí se presentan algunos datos usados para entrenar el modelo:")
+    st.write(train_df.head(10))
 
-st.subheader("Categorías Utilizadas para el Modelo")
+    st.subheader("Categorías Utilizadas para el Modelo")
 
-# Obtener y mostrar las categorías distintas
-if 'target' in train_df.columns:
-    # Crear una lista con las categorías para mostrar
-    categories_list = "<ul>" + "".join([f"<li>{category}</li>" for category in unique_targets]) + "</ul>"
-    
-    # Mostrar las categorías
-    st.write("Las categorías en las que se trabajará son las siguientes:")
-    st.markdown(categories_list, unsafe_allow_html=True)
+    # Obtener y mostrar las categorías distintas
+    if 'target' in train_df.columns:
+        # Crear una lista con las categorías para mostrar
+        categories_list = "<ul>" + "".join([f"<li>{category}</li>" for category in unique_targets]) + "</ul>"
+        
+        # Mostrar las categorías
+        st.write("Las categorías en las que se trabajará son las siguientes:")
+        st.markdown(categories_list, unsafe_allow_html=True)
 
-    # Contar el número de categorías distintas
-    num_categories = len(unique_targets)
-    st.write(f"Número de categorías distintas: {num_categories}")
+        # Contar el número de categorías distintas
+        num_categories = len(unique_targets)
+        st.write(f"Número de categorías distintas: {num_categories}")
 
-      # Contar la cantidad de noticias por cada categoría
-    category_counts = train_df['target'].value_counts()
-    
-    # Crear un gráfico de barras horizontales con Plotly
-    fig = px.bar(category_counts, 
-                 x=category_counts.values, 
-                 y=category_counts.index,
-                 orientation='h',
-                 color=category_counts.values,
-                 color_continuous_scale='Viridis',
-                 labels={'x': 'Cantidad de Noticias', 'y': 'Categoría'},
-                 title='Cantidad de Noticias por Categoría',
-                 text=category_counts.values,
-                 text_auto=True)
-    
-    # Personalizar el diseño del gráfico
-    fig.update_layout(
-        title_font_size=24,
-        xaxis_title_font_size=18,
-        yaxis_title_font_size=18,
-        legend_title_font_size=16,
-        font_size=14,
-        xaxis=dict(showgrid=False),
-        yaxis=dict(showgrid=False)
-    )
-    
+        # Contar la cantidad de noticias por cada categoría
+        category_counts = train_df['target'].value_counts()
+        
+        # Crear un gráfico de barras horizontales con Plotly
+        fig = px.bar(category_counts, 
+                    x=category_counts.values, 
+                    y=category_counts.index,
+                    orientation='h',
+                    color=category_counts.values,
+                    color_continuous_scale='Viridis',
+                    labels={'x': 'Cantidad de Noticias', 'y': 'Categoría'},
+                    title='Cantidad de Noticias por Categoría',
+                    text=category_counts.values,
+                    text_auto=True)
+        
+        # Personalizar el diseño del gráfico
+        fig.update_layout(
+            title_font_size=24,
+            xaxis_title_font_size=18,
+            yaxis_title_font_size=18,
+            legend_title_font_size=16,
+            font_size=14,
+            xaxis=dict(showgrid=False),
+            yaxis=dict(showgrid=False)
+        )
+        
+        st.plotly_chart(fig)
+    else:
+        st.write("La columna 'target' no se encuentra en el archivo CSV. Por favor, revisa el archivo.")
+
+    # Precisión del modelo
+    st.subheader('Precisión del Modelo')
+
+    accuracy_data = pd.DataFrame({
+        'Conjunto de Datos': ['Entrenamiento', 'Prueba'],
+        'Precisión': [accuracy_train, accuracy_test]
+    })
+
+    st.write("Precisión del modelo en diferentes conjuntos de datos:")
+    fig = px.bar(accuracy_data, x='Conjunto de Datos', y='Precisión', color='Conjunto de Datos', 
+                text='Precisión', height=400)
+    fig.update_traces(texttemplate='%{text:.2%}', textposition='outside')
+    fig.update_yaxes(range=[0, 1])
     st.plotly_chart(fig)
-else:
-    st.write("La columna 'target' no se encuentra en el archivo CSV. Por favor, revisa el archivo.")
 
-# Precisión del modelo
-st.subheader('Precisión del Modelo')
+    # Mostrar la matriz de confusión para los datos de entrenamiento
+    cm_train_fig = plot_confusion_matrix(cm_train_df, 'Matriz de Confusión - Datos de Entrenamiento')
+    st.plotly_chart(cm_train_fig)
 
-accuracy_data = pd.DataFrame({
-    'Conjunto de Datos': ['Entrenamiento', 'Prueba'],
-    'Precisión': [accuracy_train, accuracy_test]
-})
+    # Mostrar la matriz de confusión para los datos de prueba
+    cm_test_fig = plot_confusion_matrix(cm_test_df, 'Matriz de Confusión - Datos de Prueba')
+    st.plotly_chart(cm_test_fig)
 
-st.write("Precisión del modelo en diferentes conjuntos de datos:")
-fig = px.bar(accuracy_data, x='Conjunto de Datos', y='Precisión', color='Conjunto de Datos', 
-             text='Precisión', height=400)
-fig.update_traces(texttemplate='%{text:.2%}', textposition='outside')
-fig.update_yaxes(range=[0, 1])
-st.plotly_chart(fig)
+# Tab 3: Interacción con el Modelo
+with tab3:
+    # Sección 3: Interacción con el Modelo
+    st.header('Modelo Clasificador de Noticias')
+    st.write("""
+    Para clasificar nuevas noticias, utilice la interfaz que se proporciona a continuación. Ingrese la URL o el texto de la noticia, y el modelo automático clasificará la noticia según las características de su contenido.
+    """)
 
-# Mostrar la matriz de confusión para los datos de entrenamiento
-cm_train_fig = plot_confusion_matrix(cm_train_df, 'Matriz de Confusión - Datos de Entrenamiento')
-st.plotly_chart(cm_train_fig)
+    # Acordeón para el método de URL de Reddit
+    with st.expander("Clasificar por URL de Reddit"):
+        reddit_url = st.text_input("Ingresa la URL de Reddit:")
+        if st.button('Clasificar URL'):
+            if reddit_url:
+                st.write(f"Clasificando la URL: *{reddit_url}*")
 
-# Mostrar la matriz de confusión para los datos de prueba
-cm_test_fig = plot_confusion_matrix(cm_test_df, 'Matriz de Confusión - Datos de Prueba')
-st.plotly_chart(cm_test_fig)
+                #Llamando al modelo para clasificar la URL del post
+                prediction = predict_from_url(reddit_url)
 
-# Sección 4: Interacción con el Modelo
-st.header('Modelo Clasificador de Noticias')
-st.write("""
-Para clasificar nuevas noticias, utilice la interfaz que se proporciona a continuación. Ingrese la URL o el texto de la noticia, y el modelo automático clasificará la noticia según las características de su contenido.
-""")
+                st.subheader('Categoría Predicha')
+                st.write(f'La noticia pertenece a la categoría: **{prediction}**')
+            else:
+                st.write("Por favor, ingresa una URL válida.")
 
-# Acordeón para el método de URL de Reddit
-with st.expander("Clasificar por URL de Reddit"):
-    reddit_url = st.text_input("Ingresa la URL de Reddit:")
-    if st.button('Clasificar URL'):
-        if reddit_url:
-            st.write(f"Clasificando la URL: *{reddit_url}*")
+    # Acordeón para el método de texto
+    with st.expander("Clasificar por Texto"):
+        user_input = st.text_area("Ingresa el texto de la noticia que deseas clasificar:")
+        if st.button('Clasificar Texto'):
+            if user_input:
+                #Llamando al modelo para clasificar el texto
+                prediction = predict_from_text(user_input)
 
-            #Llamando al modelo para clasificar la URL del post
-            prediction = predict_from_url(reddit_url)
+                # Mostrar la predicción
+                st.subheader('Categoría Predicha')
+                st.write(f'La noticia pertenece a la categoría: **{prediction}**')
+            else:
+                st.write("Por favor, ingresa el texto de la noticia.")
+with tab4:
+    # Nueva sección de Resultados
+    st.subheader('Resultados')
 
-            st.subheader('Categoría Predicha')
-            st.write(f'La noticia pertenece a la categoría: **{prediction}**')
-        else:
-            st.write("Por favor, ingresa una URL válida.")
+    # Obtener la categoría más frecuente
+    most_common_category = category_counts.idxmax()
+    most_common_count = category_counts.max()
 
-# Acordeón para el método de texto
-with st.expander("Clasificar por Texto"):
-    user_input = st.text_area("Ingresa el texto de la noticia que deseas clasificar:")
-    if st.button('Clasificar Texto'):
-        if user_input:
-            #Llamando al modelo para clasificar el texto
-            prediction = predict_from_text(user_input)
+    st.write("En base a los datos de entrenamiento que obtuvimos de **Reddit**, la categoría de noticias sobre la cual más se habla es la siguiente:")
+    st.write(f"**Categoría predominante:** {most_common_category}")
+    st.write(f"**Número de noticias clasificadas:** {most_common_count}")
 
-            # Mostrar la predicción
-            st.subheader('Categoría Predicha')
-            st.write(f'La noticia pertenece a la categoría: **{prediction}**')
-        else:
-            st.write("Por favor, ingresa el texto de la noticia.")
+with tab5:
+    st.header('Acerca de')
+    st.write("""
+    Este proyecto fue desarrollado por:
 
-# Nueva sección de Resultados
-st.subheader('Resultados')
+    - **David de la Rosa**
+    - **Juan Guzman**
+    - **Gabriela Cabrera**
+    - **Dalvin Molina**
 
-# Obtener la categoría más frecuente
-most_common_category = category_counts.idxmax()
-most_common_count = category_counts.max()
+    Somos un equipo comprometido con el análisis y la clasificación de contenidos mediáticos en el contexto latinoamericano, utilizando las últimas técnicas en aprendizaje automático.
 
-st.write("En base a los datos de entrenamiento que obtuvimos de **Reddit**, la categoría de noticias sobre la cual más se habla es la siguiente:")
-st.write(f"**Categoría predominante:** {most_common_category}")
-st.write(f"**Número de noticias clasificadas:** {most_common_count}")
-
-st.header('Acerca de')
-st.write("""
-Este proyecto fue desarrollado por:
-
-- **David de la Rosa**
-- **Juan Guzman**
-- **Gabriela Cabrera**
-- **Dalvin Molina**
-
-Somos un equipo comprometido con el análisis y la clasificación de contenidos mediáticos en el contexto latinoamericano, utilizando las últimas técnicas en aprendizaje automático.
-
-Gracias por compartirnos sus conocimientos en este viaje por el mundo de los datos y la tecnología.
-""")
+    Gracias por compartirnos sus conocimientos en este viaje por el mundo de los datos y la tecnología.
+    """)
